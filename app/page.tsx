6d9680b3de299bd59853716a83defa3dc08b0162
Paul Alwin <paulalwin91@gmail.com>
2025-03-09 18:45:52 -0400

    Adding Header and Footer to the application (#3)
    * Adding Header and Footer to the application
    
    * Working App Shell With Light Dark Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
 
 export default function Home() {
-  return
-  (
-    <section className='py-24'>
-      <div className="container">
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
         <h1 className="text-3xl font-bold">Portfolio</h1>
       </div>
     </section>
