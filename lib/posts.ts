b46f281255cdcf8f6f2f9265dfb760cb2513cbb3
Paul Alwin <paulalwin91@gmail.com>
2025-03-15 21:49:59 -0400

    All Posts Page

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,10 +1,8 @@
+import fs from 'fs'
 import path from 'path'
-import fs from 'fs/promises'
 import matter from 'gray-matter'
-import { metadata } from '@/app/layout'
 
 const rootDirectory = path.join(process.cwd(), 'content', 'posts')
-
 
 export type Post = {
     metadata: PostMetadata
@@ -17,19 +15,44 @@ export type PostMetadata = {
     image?: string
     author?: string
     publishedAt?: string
-    slug?: string
+    slug: string
 }
 
 export async function getPostBySlug(slug: string): Promise<Post | null> {
     try {
-
         const filePath = path.join(rootDirectory, `${slug}.mdx`)
-        const fileContents = await fs.readFile(filePath, 'utf8');
-        const { data, content } = matter(fileContents)
+        const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
+        const { data, content } = matter(fileContent)
         return { metadata: { ...data, slug }, content }
-
     } catch (error) {
-        console.error(`Error getting post by slug: ${slug}`, error)
         return null
     }
+}
+
+export async function getPosts(limit?: number): Promise<PostMetadata[]> {
+    const files = fs.readdirSync(rootDirectory)
+
+    const posts = files
+        .map(file => getPostMetadata(file))
+        .sort((a, b) => {
+            if (new Date(a.publishedAt ?? '') < new Date(b.publishedAt ?? '')) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+
+    if (limit) {
+        return posts.slice(0, limit)
+    }
+
+    return posts
+}
+
+export function getPostMetadata(filepath: string): PostMetadata {
+    const slug = filepath.replace(/\.mdx$/, '')
+    const filePath = path.join(rootDirectory, filepath)
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
+    const { data } = matter(fileContent)
+    return { ...data, slug }
 }
