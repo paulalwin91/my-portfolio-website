0b69787699758d3a808165a6499a3a1b35a22a4c
Paul Alwin <paulalwin91@gmail.com>
2025-03-27 23:52:50 -0400

    Updated my about me page

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -18,24 +18,17 @@ export default async function AboutMe() {
                         />
                     </div>
                     <div className="flex-1">
-                        <p>Hello! I’m Paul Alwin, a software developer based in Montreal, currently working at Société Générale.
-                            With over a decade of experience in the software industry, my journey began in India, where I developed a passion for technology and innovation.
-                            In 2017, I moved to Canada to pursue a Master’s in Software Engineering, which opened up new opportunities in the tech world.</p>
+                        <p>Hello! I’m Paul Alwin, a senior software developer in .net based in Montreal, currently working at Société Générale.
+                            With over a decade of experience in the software industry, my journey began in India, where I developed a passion for technology.
+                            In 2017, I moved here to Canada to pursue a Master’s in Software Engineering, and now working in Societe Generale in Montreal.</p>
+                        <br />                        
+                        <p> Majority of my expertise lies in back-end development, specializing in C#, ASP.NET Core, and SQL,
+                            with extensive experience in CI/CD pipelines using Jenkins. I also am working on my front end skills by learning 
+                            React and Next js - for example this website is build on Next.js hosted on Vercel.</p>
                         <br />
-                        <p>During my studies, I interned and worked part-time at Genetec,
-                            where I managed a project independently, honing my skills in real-world scenarios.
-                            This experience led me to Quantum Consulting, where I worked with clients like Société Générale.
-                            In 2021, I was thrilled to join Société Générale as a full-time employee.</p>
-                        <br />
-                        <p>My expertise lies in back-end development, specializing in C#, ASP.NET Core, and SQL,
-                            with extensive experience in CI/CD pipelines using Jenkins. Throughout my career, I’ve had the privilege
-                            of mentoring interns and being involved in the interview process, fostering a collaborative
-                            and growth-oriented environment.</p>
-                        <br />
-                        <p>I’m passionate about continuous learning and applying best practices,
-                            SOLID principles, and design patterns in my work. While my focus has been on back-end development,
-                            I’m also expanding my skills into front-end technologies.
-                            This website you’re visiting is built with Next.js, reflecting my commitment to modern web development.</p>
+                        <p>I’m passionate about technology and am always trying to improve my skills by learning and keeping myself updated 
+                            with the latest advancements in the software industry. At work I have a growth mindset by proactivly going for challenging tasks 
+                            and be on the lookout for improving existing processes.</p>
                         <br />
                         <p>This year, I’m aiming to enhance my skills further by obtaining certifications in Azure Cloud.</p>
                         <br />
