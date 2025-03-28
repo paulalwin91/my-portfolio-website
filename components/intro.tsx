a2fa09265fe8b9b2b167d1ff8a9d9a06492fdc27
Paul Alwin <paulalwin91@gmail.com>
2025-03-28 00:11:02 -0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ export default function Intro() {
                 <h1 className='title no-underline'>Hey, I&#39;m Paul.</h1>
                 <p className='mt-3 mb-7 font-light text-muted-foreground'>
                     I&#39;m a senior software engineer in .Net with over 10 years of experience based in Montreal, Canada -
-                    passionate about learning new technologies and sharing knowledge with others. Click below for more details and my resume.
+                    passionate about learning new technologies and sharing knowledge with others.
                 </p>
                 <p className='mt-3 font-light text-muted-foreground'>
                     <strong><i>Key Skills - C# | ASP.NET Core | SQL | CI/CD (Jenkins) | Powershell | Azure | React | Next.js | Angular</i></strong>
