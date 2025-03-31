5fc1a8d7f0ed74866113dede7d108c8b48ee3170
Paul Alwin <paulalwin91@gmail.com>
2025-03-30 20:33:22 -0400

    Change the ToDo App Description

--- components/todo-app/NewTodoForm.jsx
+++ components/todo-app/NewTodoForm.jsx
@@ -20,7 +20,7 @@ export default function NewTodoForm({ handleAdd })
         <form className="new-item-form" onSubmit={handleSubmit}
         >
             <div className="form-row">
-                <label htmlFor="item">New Item</label>
+                <label htmlFor="item">Add a new Item below!</label>
                 <input value={newItem}
                     onChange={e => setNewItem(e.target.value)} type="text" id="item" />
             </div>
